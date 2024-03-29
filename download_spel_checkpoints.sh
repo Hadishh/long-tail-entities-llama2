b9d71935bc93a0cf72e3a4f258f1c3a98661c841
Hadi Sheikhi <hadsheikhi@gmail.com>
2024-03-28 18:13:00 -0600

    fix download spel checkpoints bug

--- download_spel_checkpoints.sh
+++ download_spel_checkpoints.sh
@@ -1,5 +1,6 @@
 rm -rf .checkpoints
 mkdir .checkpoints
+cd .checkpoints
 
-cd .checkpoints && gdown 1MmBlJOxWj7thLmWUGuogIkByLAi3Qem_
-cd .checkpoints && gdown 1eugh_tluJ7-6o8SdC-CdWiKW-5DomwPJ
+gdown 1MmBlJOxWj7thLmWUGuogIkByLAi3Qem_ #spel base
+gdown 1eugh_tluJ7-6o8SdC-CdWiKW-5DomwPJ #spel large
